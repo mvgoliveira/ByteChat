ad45d48419e740dfe279a62d76c202e42bcf01ce
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2021-11-05 22:08:36 -0300

    Added Home Page

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import { Container } from './styles';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import {Container} from "./styles";
+
+import { useAuth } from "../../hooks/useAuth";
 
 export function Home() {
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+  
+  const history = useHistory();
+  
+  const { 
+    login
+  } = useAuth();
+
+  function handleRoomIDInputChange(event) {
+    setRoomId(event.currentTarget.value);
+  }
+
+  function handleNameInputChange(event) {
+    setUsername(event.currentTarget.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (username !== "" && roomId !== "") {
+      login(username);
+      history.push(`/room/${roomId}`);
+    }
+  }
+
   return (
     <Container>
-      <h1>
-        Home
-      </h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="digite seu nome"
+          value={username}
+          onChange={handleNameInputChange}
+        />
+        <input
+          type="text"
+          placeholder="digite o código da sala"
+          value={roomId}
+          onChange={handleRoomIDInputChange}
+        />
+        <button type="submit">Entrar na sala</button>
+      </form>
     </Container>
   );
 }
--- frontend/src/pages/Home/styles.js
+++ frontend/src/pages/Home/styles.js
@@ -1,9 +1,60 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-  background: #333333;
-  color: #fff;
-  width: 100vw;
+  display: flex;
+  flex-direction: column;
   height: 100vh;
+  align-items: center;
+  justify-content: center;
 
+  ul {
+    margin-bottom: 20px;
+    background: #F0F0F0;
+    padding: 20px;
+
+    li {
+      text-decoration: none;
+      list-style: none;
+      text-align: center;
+      padding: 15px;
+      border-radius: 8px;
+      border: 1px solid #bbbbbb;
+      margin-bottom: 5px;
+      margin-right: 200px;
+      background: #b6ccf0;
+    }
+
+    .user1 {
+      margin-right: 0px;
+      margin-left: 200px;
+    }
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    
+    input {
+      margin-bottom: 5px;
+    }
+
+    button {
+      padding: 15px;
+      border-radius: 8px;
+      border: 1px solid #bbbbbb;
+      cursor: pointer;
+    }
+  }
+
+  > button {
+    padding: 15px;
+    border-radius: 8px;
+    border: 1px solid #bbbbbb;
+    cursor: pointer;
+    margin-top: 15px;
+  }
+
+  p {
+    margin-top: 15px;
+  }
 `;
