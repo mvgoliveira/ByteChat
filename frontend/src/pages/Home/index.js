ac334faa187946b322f4513ec76442f9f6c57cab
Guilherme Moura <guilhermemouradsantos1@gmail.com>
2021-11-09 14:03:04 -0300

    a

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -4,10 +4,7 @@ import { Container } from './styles';
 export function Home() {
   return (
     <Container>
-      
-      <h1>
-        Home
-      </h1>
+      <h1>Hello World</h1>
     </Container>
   );
 }
